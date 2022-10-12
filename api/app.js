bfc0f7fb769a1f9a62fe7db7a9062bbdae238b24
yuyuyuno <fuwafuwayuya@gmail.com>
2022-10-12 18:16:20 +0300

    needless routes & libs deleted

--- api/app.js
+++ api/app.js
@@ -1,29 +1,13 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const cors = require('cors');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const newRoute = require('./routes/newRoute');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(cors());
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/newroute', newRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
