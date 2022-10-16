11790de539b052fc2a8c824218dd44b03f1225c0
yuyuyuno <fuwafuwayuya@gmail.com>
2022-10-16 03:40:38 +0300

    moved functions to helpers dir

--- api/app.js
+++ api/app.js
@@ -1,7 +1,9 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const http = require('http');
+
+const { onListening, errorHandler, onError, normalizePort } = require('./helpers');
 
 const app = express();
 
@@ -9,8 +11,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   errorHandler(next, 404);
 });
+
+const port = normalizePort(process.env.PORT || '9000');
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
 module.exports = app;
---
+++ api/helpers/index.js
@@ -0,0 +1,6 @@
+const errorHandler = require('./errorHandler');
+const onListening = require('./onListening');
+const onError = require('./onError');
+const normalizePort = require('./normalizePort');
+
+module.exports = { errorHandler, onListening, onError, normalizePort };
