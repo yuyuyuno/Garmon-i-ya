30683dd7ca22764723751ceb29eb17644bf67622
yuyuyuno <fuwafuwayuya@gmail.com>
2022-11-01 10:35:20 +0300

    Piano component created

--- client/src/pages/MelodyInput/components/Sequencer/Piano/Piano.jsx
+++ client/src/pages/MelodyInput/components/Sequencer/Piano/Piano.jsx
@@ -1,3 +1,50 @@
-import { PianoKey } from "./PianoKey";
+import { PianoKey } from './PianoKey';
 
-export const Piano = () => <div>Piano<PianoKey/></div>;
+const KEY_NAMES = [
+	'C',
+	'C#',
+	'D',
+	'D#',
+	'E',
+	'F',
+	'F#',
+	'G',
+	'G#',
+	'A',
+	'A#',
+	'B',
+];
+
+const OCTAVE_LENGTH = KEY_NAMES.length;
+
+const getRangeArray = (elementsCount) => [...new Array(elementsCount).keys()];
+
+export const Piano = (props) => {
+	const { keysCount = 48, showKeyNames = true } = props;
+
+	const isKeyBlack = (keyIndex) => {
+		const isOdd = keyIndex % 2 !== 0;
+		const isLesserThanFive = keyIndex < 5;
+		return (isOdd && isLesserThanFive) || (!isOdd && !isLesserThanFive);
+	};
+
+	const getKeyInfo = (keyIndex) => {
+		const keyIndexInOctave = keyIndex % OCTAVE_LENGTH;
+		return {
+			label: showKeyNames ? KEY_NAMES[keyIndexInOctave] : '',
+			isBlack: isKeyBlack(keyIndexInOctave),
+		};
+	};
+
+	const keysInfo = getRangeArray(keysCount).map((keyIndex) =>
+		getKeyInfo(keyIndex)
+	);
+
+	return (
+		<div>
+			{keysInfo.map((i, keyInfo) => (
+				<PianoKey key={i} {...keyInfo} />
+			))}
+		</div>
+	);
+};
