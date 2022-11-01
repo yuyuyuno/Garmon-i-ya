84a08de5b69e9f088634374532307f5be018e4e3
yuyuyuno <fuwafuwayuya@gmail.com>
2022-11-01 12:06:33 +0300

    piano key component created

--- client/src/pages/MelodyInput/components/Sequencer/Piano/PianoKey/PianoKey.jsx
+++ client/src/pages/MelodyInput/components/Sequencer/Piano/PianoKey/PianoKey.jsx
@@ -1,1 +1,17 @@
-export const PianoKey = () => <div>Piano key</div>;
+export const PianoKey = (props) => {
+	const { label, isBlack } = props;
+
+	const keyStyles = {
+		border: 'solid 1px #000',
+		backgroundColor: isBlack ? '#000' : '#fff',
+		color: isBlack ? '#fff' : '#000',
+		textAlign: 'right',
+	};
+
+	console.log(props);
+	return (
+		<div style={keyStyles}>
+			{label} {isBlack}
+		</div>
+	);
+};
