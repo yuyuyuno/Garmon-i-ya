75204b5ee82ef395fbc21a4c2157cab90e8af1b2
yuyuyuno <fuwafuwayuya@gmail.com>
2022-11-04 23:29:58 +0300

    conditional rendering fix

--- client/src/pages/MelodyInput/components/Sequencer/Input/Input.jsx
+++ client/src/pages/MelodyInput/components/Sequencer/Input/Input.jsx
@@ -1,5 +1,6 @@
 import { Note } from './Note';
 import { getRangeArray } from '../../../../../utils';
+import { keyIndexToNote } from '../../../../../utils';
 
 export const Input = (props) => {
 	const { rowCount = 48, columnCount, inputMelody, clickHandler } = props;
@@ -15,6 +16,7 @@ export const Input = (props) => {
 		width: '30px',
 		padding: '0px',
 		borderColor: '#72768A',
+		height: '21.969px',
 	};
 
 	return (
@@ -27,9 +29,11 @@ export const Input = (props) => {
 								style={cellStyles}
 								onClick={() => clickHandler(rowIndex, columnIndex)}
 							>
-								{inputMelody[columnIndex] && (
-									<Note headOfLongNote={false} bodyOfLongNote={false} />
-								)}
+								{inputMelody[columnIndex] !== null &&
+									inputMelody[columnIndex].note ===
+										keyIndexToNote(47 - rowIndex) && (
+										<Note headOfLongNote={false} bodyOfLongNote={false} />
+									)}
 							</td>
 						))}
 					</tr>
