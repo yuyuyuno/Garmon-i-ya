e00b5c748f84e24b72ee22c7478f3d447929fd4e
yuyuyuno <fuwafuwayuya@gmail.com>
2022-11-05 19:08:07 +0300

    utils export fix

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,4 +1,2 @@
-export { getRangeArray } from './arrayUtils';
-export { KEY_NAMES } from './musicUtils';
-export { OCTAVE_LENGTH } from './musicUtils';
-export { keyIndexToNote } from './musicUtils';
+export * from './arrayUtils';
+export * from './musicUtils';
