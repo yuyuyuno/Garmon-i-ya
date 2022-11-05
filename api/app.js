9af777655e5d9ee1801f0f936d815b7981457f8e
yuyuyuno <fuwafuwayuya@gmail.com>
2022-11-06 00:40:12 +0300

    Refactoring

--- api/app.js
+++ api/app.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const http = require('http');
+
+// routes T O D O
 
 const { handleListening, errorHandler, normalizePort } = require('./helpers');
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-
 const port = normalizePort(process.env.PORT || '9000');
-app.set('port', port);
+const server = express();
 
-const server = http.createServer(app);
+server.use(cors());
+server.use(express.json());
+// server.use(routes);
+server.use(express.static(path.join(__dirname, 'public')));
 
-server.listen(port);
-server.on('error', errorHandler);
-server.on('listening', () => handleListening(server));
+server.listen(port, () => handleListening(server));
 
-module.exports = app;
+module.exports = server;
--- api/helpers/index.js
+++ api/helpers/index.js
@@ -1,6 +1,5 @@
 const errorHandler = require('./errorHandler');
 const handleListening = require('./handleListening');
-const errorHandler = require('./errorHandler');
 const normalizePort = require('./normalizePort');
 
-module.exports = { errorHandler, handleListening, errorHandler, normalizePort };
+module.exports = { handleListening, errorHandler, normalizePort };
