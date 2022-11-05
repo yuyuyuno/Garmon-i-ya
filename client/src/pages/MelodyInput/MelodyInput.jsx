6a5265e3f0be56064e54bf6161416e4173623b09
yuyuyuno <fuwafuwayuya@gmail.com>
2022-11-05 23:20:39 +0300

    Refactoring WIP

--- client/src/pages/MelodyInput/MelodyInput.jsx
+++ client/src/pages/MelodyInput/MelodyInput.jsx
@@ -1,78 +1,90 @@
 import { useState } from 'react';
 
 import { Button } from '../../components/Button';
-import { getRangeArray } from '../../utils';
 import { Sequencer } from './components/Sequencer';
+
+import { getRangeArray } from '../../utils';
 
 export const MelodyInput = () => {
 	const [inputMelody, setInputMelody] = useState(new Array(24).fill(null));
 	const [noteLength, setNoteLength] = useState('quarter');
+	let nullCounter = 0;
+	let longNoteCounter = 1;
 
-	const addNote = (name, length, noteArray) => {
-		noteArray.push({
-			noteName: name,
-			noteLength: length,
+	const addNoteToMeasure = (measureNotes, noteInfo) => {
+		measureNotes.push({
+			noteName: noteInfo[0],
+			noteLength: noteInfo[1],
 		});
 	};
 
-	const ResButtonClickHandler = () => {
-		const melodyArray = new Array(inputMelody.length / 8)
-			.fill([])
-			.map((measureNotes, measureIndex) => {
-				let nullCounter = 0;
-				let longNoteCounter = 1;
-				measureNotes = [];
-				getRangeArray(8, measureIndex * 8).forEach((melodyNoteIndex) => {
-					if (!inputMelody[melodyNoteIndex]) {
-						++nullCounter;
-						if (
-							melodyNoteIndex !== measureIndex * 8 &&
-							inputMelody[melodyNoteIndex - 1]
-						) {
-							addNote(
-								inputMelody[melodyNoteIndex - 1].note,
-								longNoteCounter,
-								measureNotes
-							);
-							longNoteCounter = 1;
-						}
-					} else {
-						if (melodyNoteIndex !== measureIndex * 8 && nullCounter) {
-							addNote('stop', nullCounter, measureNotes);
-							nullCounter = 0;
-						} else {
-							if (inputMelody[melodyNoteIndex]?.isHeld) {
-								++longNoteCounter;
-							} else {
-								if (
-									melodyNoteIndex !== measureIndex * 8 &&
-									inputMelody[melodyNoteIndex - 1]?.isHeld
-								) {
-									addNote(
-										inputMelody[melodyNoteIndex - 1].note,
-										longNoteCounter,
-										measureNotes
-									);
-									longNoteCounter = 1;
-								}
-							}
-						}
-					}
-					if (melodyNoteIndex === (measureIndex + 1) * 8 - 1) {
-						addNote(
-							inputMelody[melodyNoteIndex]
-								? inputMelody[melodyNoteIndex].note
-								: 'stop',
-							inputMelody[melodyNoteIndex] ? longNoteCounter : nullCounter,
-							measureNotes
-						);
-						longNoteCounter = 1;
-						nullCounter = 0;
-					}
-				});
+	const handleLastMeasureNote = (
+		measureNotes,
+		measureIndex,
+		melodyNoteIndex
+	) => {
+		const isLastCell = melodyNoteIndex === (measureIndex + 1) * 8 - 1;
+		if (!isLastCell) {
+			return;
+		}
 
-				return measureNotes;
-			});
+		addNoteToMeasure(measureNotes, [
+			inputMelody[melodyNoteIndex] ? inputMelody[melodyNoteIndex].note : 'stop',
+			inputMelody[melodyNoteIndex] ? longNoteCounter : nullCounter,
+		]);
+
+		longNoteCounter = 1;
+		nullCounter = 0;
+	};
+
+	const getCellNote = (melodyNoteIndex, measureIndex) => {
+		const measureNotes = [];
+
+		const isFirstCell = melodyNoteIndex === measureIndex * 8;
+		const prevCell = inputMelody[melodyNoteIndex - 1];
+
+		if (!inputMelody[melodyNoteIndex]) {
+			++nullCounter;
+			if (!isFirstCell && prevCell) {
+				addNoteToMeasure(measureNotes, [prevCell.note, longNoteCounter]);
+				longNoteCounter = 1;
+			}
+		}
+
+		if (inputMelody[melodyNoteIndex]) {
+			if (!isFirstCell && nullCounter) {
+				addNoteToMeasure(measureNotes, ['stop', nullCounter]);
+				nullCounter = 0;
+			} else {
+				if (inputMelody[melodyNoteIndex]?.isHeld) {
+					++longNoteCounter;
+				} else {
+					if (!isFirstCell && prevCell?.isHeld) {
+						addNoteToMeasure(measureNotes, [prevCell.note, longNoteCounter]);
+						longNoteCounter = 1;
+					}
+				}
+			}
+		}
+
+		handleLastMeasureNote(measureNotes, measureIndex, melodyNoteIndex);
+
+		console.log('NOTE OR 2', measureNotes);
+		return measureNotes;
+	};
+
+	const handleResButtonClick = () => {
+		const measureCount = inputMelody.length / 8;
+		const melodyArray = getRangeArray(measureCount).map((measureIndex) =>
+			getRangeArray(8, measureIndex * 8).reduce(
+				(measureNotes, melodyNoteIndex) => {
+					measureNotes.push(...getCellNote(melodyNoteIndex, measureIndex));
+					return measureNotes;
+				},
+				[]
+			)
+		);
+
 		console.log(melodyArray);
 	};
 
@@ -85,7 +97,7 @@ export const MelodyInput = () => {
 				noteLength={noteLength}
 				setInputMelody={setInputMelody}
 			/>
-			<Button clickHandler={ResButtonClickHandler} label={'harmonize'} />
+			<Button clickHandler={handleResButtonClick} label={'harmonize'} />
 		</div>
 	);
 };
