60642b190af80e206380d4483d5041ddfa468f0b
yuyuyuno <fuwafuwayuya@gmail.com>
2022-11-06 16:10:05 +0300

    getLastMeasureNote, getNoteBeforeEmptyCell, getNoteFromPrevCell, getStopNote to utils

--- client/src/pages/MelodyInput/MelodyInput.jsx
+++ client/src/pages/MelodyInput/MelodyInput.jsx
@@ -4,7 +4,14 @@ import axios from '../../axiosWrapper';
 import { Button } from '../../components/Button';
 import { Sequencer } from './components/Sequencer';
 
-import { getRangeArray, pushIfNotNull, noteInfoToNoteObj } from '../../utils';
+import {
+	getRangeArray,
+	pushIfNotNull,
+	getLastMeasureNote,
+	getNoteBeforeEmptyCell,
+	getNoteFromPrevCell,
+	getStopNote,
+} from '../../utils';
 
 export const MelodyInput = (props) => {
 	const { handleGetHarmonized } = props;
@@ -12,88 +19,74 @@ export const MelodyInput = (props) => {
 	const [inputMelody, setInputMelody] = useState(new Array(24).fill(null));
 	const [noteLength, setNoteLength] = useState('quarter');
 
-	let nullCounter = 0;
-	let longNoteCounter = 1;
-
-	const getLastMeasureNote = (measureIndex, melodyNoteIndex) => {
-		const isLastCell = melodyNoteIndex === (measureIndex + 1) * 8 - 1;
-		if (!isLastCell) {
-			return;
-		}
-
-		const noteInfo = inputMelody[melodyNoteIndex];
-		const lastNote = noteInfoToNoteObj([
-			noteInfo?.note || 'stop',
-			noteInfo ? longNoteCounter : nullCounter,
-		]);
-
-		longNoteCounter = 1;
-		nullCounter = 0;
-
-		return lastNote;
-	};
-
-	const getNoteBeforeEmptyCell = (isFirstCell, prevCell) => {
-		++nullCounter;
-
-		if (isFirstCell || !prevCell) {
-			return;
-		}
-
-		const cellNote = noteInfoToNoteObj([prevCell.note, longNoteCounter]);
-		longNoteCounter = 1;
-
-		return cellNote;
-	};
-
-	const getNoteFromPrevCell = (prevCell) => {
-		const cellNote = noteInfoToNoteObj([prevCell.note, longNoteCounter]);
-		longNoteCounter = 1;
-
-		return cellNote;
-	};
-
-	const getStopNote = () => {
-		const cellNote = noteInfoToNoteObj(['stop', nullCounter]);
-		nullCounter = 0;
-
-		return cellNote;
-	};
-
-	const getCellNote = (melodyNoteIndex, measureIndex) => {
-		const isFirstCell = melodyNoteIndex === measureIndex * 8;
-		const prevCell = inputMelody[melodyNoteIndex - 1];
-
-		if (!inputMelody[melodyNoteIndex]) {
-			return getNoteBeforeEmptyCell(isFirstCell, prevCell);
-		}
-
-		if (!isFirstCell && nullCounter) {
-			return getStopNote();
-		}
-
-		if (inputMelody[melodyNoteIndex]?.isHeld) {
-			++longNoteCounter;
-			return;
-		}
-
-		if (!isFirstCell && prevCell) {
-			return getNoteFromPrevCell(prevCell);
-		}
-	};
-
 	const adaptMelodyArray = () => {
 		const measureCount = inputMelody.length / 8;
+		let nullCounter = 0;
+		let longNoteCounter = 1;
+
+		const getCellNote = (melodyNoteIndex, measureIndex) => {
+			const isFirstCell = melodyNoteIndex === measureIndex * 8;
+			const prevCell = inputMelody[melodyNoteIndex - 1];
+
+			if (!inputMelody[melodyNoteIndex]) {
+				const noteInfo = getNoteBeforeEmptyCell(
+					isFirstCell,
+					prevCell,
+					longNoteCounter
+				);
+
+				++nullCounter;
+				if (noteInfo) {
+					longNoteCounter = 1;
+				}
+
+				return noteInfo;
+			}
+
+			if (!isFirstCell && nullCounter) {
+				const noteInfo = getStopNote(nullCounter);
+				nullCounter = 0;
+
+				return noteInfo;
+			}
+
+			if (inputMelody[melodyNoteIndex]?.isHeld) {
+				++longNoteCounter;
+				return;
+			}
+
+			if (!isFirstCell && prevCell) {
+				const noteInfo = getNoteFromPrevCell(prevCell, longNoteCounter);
+				longNoteCounter = 1;
+
+				return noteInfo;
+			}
+		};
+
+		const getMeasureNotes = (measureNotes, melodyNoteIndex, measureIndex) => {
+			const cellNote = getCellNote(melodyNoteIndex, measureIndex);
+			const lastMeasureNote = getLastMeasureNote(
+				inputMelody,
+				measureIndex,
+				melodyNoteIndex,
+				nullCounter,
+				longNoteCounter
+			);
+
+			if (lastMeasureNote) {
+				longNoteCounter = 1;
+				nullCounter = 0;
+			}
+
+			pushIfNotNull(measureNotes, [cellNote, lastMeasureNote]);
+
+			return measureNotes;
+		};
+
 		const melodyArray = getRangeArray(measureCount).map((measureIndex) =>
 			getRangeArray(8, measureIndex * 8).reduce(
-				(measureNotes, melodyNoteIndex) => {
-					pushIfNotNull(measureNotes, [
-						getCellNote(melodyNoteIndex, measureIndex),
-						getLastMeasureNote(measureIndex, melodyNoteIndex),
-					]);
-
-					return measureNotes;
-				},
+				(measureNotes, melodyNoteIndex) =>
+					getMeasureNotes(measureNotes, melodyNoteIndex, measureIndex),
 				[]
 			)
 		);
--- client/src/utils/musicUtils.js
+++ client/src/utils/musicUtils.js
@@ -10,3 +10,42 @@ export const noteInfoToNoteObj = (noteInfo) => ({
 	noteName: noteInfo[0],
 	noteLength: noteInfo[1],
 });
+
+export const getLastMeasureNote = (
+	inputMelody,
+	measureIndex,
+	melodyNoteIndex,
+	nullCounter,
+	longNoteCounter
+) => {
+	const isLastCell = melodyNoteIndex === (measureIndex + 1) * 8 - 1;
+	if (!isLastCell) {
+		return;
+	}
+
+	const noteInfo = inputMelody[melodyNoteIndex];
+	const lastNote = noteInfoToNoteObj([
+		noteInfo?.note || 'stop',
+		noteInfo ? longNoteCounter : nullCounter,
+	]);
+
+	return lastNote;
+};
+
+export const getNoteBeforeEmptyCell = (
+	isFirstCell,
+	prevCell,
+	longNoteCounter
+) => {
+	if (isFirstCell || !prevCell) {
+		return;
+	}
+
+	return noteInfoToNoteObj([prevCell.note, longNoteCounter]);
+};
+
+export const getNoteFromPrevCell = (prevCell, longNoteCounter) =>
+	noteInfoToNoteObj([prevCell.note, longNoteCounter]);
+
+export const getStopNote = (nullCounter) =>
+	noteInfoToNoteObj(['stop', nullCounter]);
