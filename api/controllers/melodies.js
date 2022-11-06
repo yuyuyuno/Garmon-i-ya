d694f3864b3e6a068b1e250385614b68499d7d6d
yuyuyuno <fuwafuwayuya@gmail.com>
2022-11-06 14:51:18 +0300

    Finished harmonizeMelody method of melodies controller

--- api/controllers/melodies.js
+++ api/controllers/melodies.js
@@ -1,5 +1,27 @@
+const Melody = require('../logic/melodies/melody');
+
+/*
+	req.body contains melody and later options which should be passed to harmonize method. 
+	To understand the structure, melody is of type Melody (wow!!!).
+	In TS syntax it's like the following...
+
+	type Melody = Measure[];
+	type Measure = Note[];
+	type Note = {
+		noteName: string;
+		noteLength: number;
+	};
+	
+	noteName consists of note letter and octave number (like A5);
+	noteLength is a note length in count of 1/8;
+*/
 const harmonizeMelody = (req, res) => {
-	console.log('TEST harmonizeMelody body params', req.body);
+	const { melody } = req.body;
+	const harmonizedMelody = new Melody(melody).harmonize().notes;
+
+	res.status(200).send({
+		harmonizedMelody,
+	});
 };
 
 module.exports = {
