025ab8564177ddac8aaef7bd14b7e9f65955284a
yuyuyuno <fuwafuwayuya@gmail.com>
2022-11-12 21:57:06 +0300

    output logic wip

--- api/logic/melodies/melody.js
+++ api/logic/melodies/melody.js
@@ -2,7 +2,15 @@ const Harmonizer = require('./harmonizer');
 
 class Melody {
 	#notes;
-	#removeStops = removeStops;
+
+	removeStops = removeStops;
+	harmonize = harmonize;
+	getAbc = getAbc;
+
+	static getMeasureAbc = getMeasureAbc;
+	static getNoteAbc = getNoteAbc;
+	static getPauseAbc = getPauseAbc;
+	static isStop = isStop;
 
 	constructor(melody) {
 		this.#notes = melody;
@@ -10,15 +18,6 @@ class Melody {
 
 	get notes() {
 		return this.#notes;
-	}
-
-	harmonize(options) {
-		const filteredNotes = this.#removeStops();
-		const harmonizer = new Harmonizer(filteredNotes);
-
-		// The magic of harmonization later
-
-		return this;
 	}
 }
 
@@ -28,6 +27,62 @@ function removeStops() {
 	);
 
 	return melodyWithoutStops;
+}
+
+function harmonize(options) {
+	const filteredNotes = this.removeStops();
+	const harmonizer = new Harmonizer(filteredNotes);
+
+	// The magic of harmonization later
+
+	return this;
+}
+
+function getAbc() {
+	console.log('Given notes: ', this.notes);
+	const notesAbc = this.notes.reduce(
+		(resAbc, curMeasure) => resAbc + getMeasureAbc(curMeasure) + '|',
+		''
+	);
+	console.log(notesAbc);
+	return notesAbc;
+}
+
+function getMeasureAbc(measure) {
+	const measureAbc = measure.reduce((resMeasureAbc, curNote) => {
+		let curNoteAbc;
+		if (isStop(curNote)) {
+			curNoteAbc = getPauseAbc(curNote.length);
+		} else {
+			curNoteAbc = getNoteAbc(curNote);
+		}
+
+		return resMeasureAbc + curNoteAbc;
+	}, '');
+	return measureAbc;
+}
+
+function getNoteAbc(noteInfo) {
+	const octaveNumber = noteInfo.noteName.slice(-1);
+	let note;
+	note = noteInfo.noteName.slice(0, -1);
+	switch (octaveNumber) {
+		case '3':
+			note = `${noteInfo.noteName},`;
+		case '5':
+			note = noteInfo.noteName.toLowerCase();
+		case '6':
+			note = `${noteInfo.noteName}'`;
+	}
+	return note + noteInfo.noteLength;
+}
+
+function getPauseAbc(pauseLength) {
+	return 'z' + pauseLength;
+}
+
+function isStop(noteInfo) {
+	return noteInfo.noteName === 'stop';
 }
 
 module.exports = Melody;
