439c2d96dffd230e332b1ddcb297472d7a36123a
yuyuyuno <fuwafuwayuya@gmail.com>
2022-11-27 05:37:14 +0300

    some refactoring

--- api/logic/melodies/harmonizer.js
+++ api/logic/melodies/harmonizer.js
@@ -8,6 +8,7 @@ class Harmonizer {
 	getKeyNotes = getKeyNotes;
 	getChordsAbc = getChordsAbc;
 	getArpeggioAbc = getArpeggioAbc;
+	getRootNotes = getRootNotes;
 	majorKey = [0, 2, 4, 5, 7, 9, 11];
 	minorKey = [0, 2, 3, 5, 7, 8, 10];
 
@@ -18,56 +19,6 @@ class Harmonizer {
 	get melody() {
 		return this.#melody;
 	}
-}
-
-function getKeyNotes() {
-	const keyNotes = [];
-	const schmuckler = new Schmuckler(this.melody);
-	const tonality = schmuckler.krumhanslSchmuckler();
-	const tonicIndex = NOTE_NAMES.findIndex((elem) => elem === tonality.tonic);
-
-	if (tonality.isMajor) {
-		for (let i = 0; i < 7; ++i) {
-			keyNotes.push(
-				NOTE_NAMES[(tonicIndex + this.majorKey[i]) % NOTE_NAMES.length]
-			);
-		}
-	} else {
-		for (let i = 0; i < 7; ++i) {
-			keyNotes.push(
-				NOTE_NAMES[(tonicIndex + this.minorKey[i]) % NOTE_NAMES.length]
-			);
-		}
-	}
-
-	return keyNotes;
-}
-
-function getChordNotes() {
-	const keyNotes = this.getKeyNotes();
-	const chordNotes = this.melody
-		.map((measure) =>
-			measure.reduce((prev, cur) =>
-				prev.noteLength >= cur.noteLength ? prev : cur
-			)
-		)
-		.map((note) => {
-			const noteName = note.noteName.slice(0, -1);
-			if (keyNotes.includes(noteName)) {
-				const chance = Math.random();
-				if (chance <= 0.3) {
-					return keyNotes[(keyNotes.indexOf(noteName) + 3) % keyNotes.length];
-				} else if (chance >= 0.7) {
-					return keyNotes[(keyNotes.indexOf(noteName) + 5) % keyNotes.length];
-				}
-			}
-			return noteName;
-		});
-
-	chordNotes.splice(-1);
-	chordNotes.push(keyNotes[0]);
-
-	return chordNotes;
 }
 
 function getChordsAbc() {
@@ -106,6 +57,59 @@ function getArpeggioAbc() {
 	});
 
 	return arpeggioAbc;
+}
+
+function getChordNotes() {
+	const keyNotes = this.getKeyNotes();
+	const chordNotes = this.melody
+		.map((measure) =>
+			measure.reduce((prev, cur) =>
+				prev.noteLength >= cur.noteLength ? prev : cur
+			)
+		)
+		.map((note) => getRootNotes(note, keyNotes));
+	chordNotes.splice(-1);
+	chordNotes.push(keyNotes[0]);
+
+	return chordNotes;
+}
+
+function getRootNotes(note, keyNotes) {
+	const noteName = note.noteName.slice(0, -1);
+	if (keyNotes.includes(noteName)) {
+		const chance = Math.random();
+		if (chance <= 0.3) {
+			return keyNotes[(keyNotes.indexOf(noteName) + 3) % keyNotes.length];
+		} else if (chance >= 0.7) {
+			return keyNotes[(keyNotes.indexOf(noteName) + 5) % keyNotes.length];
+		}
+	}
+	return noteName;
+}
+
+function getRandomChordAbc() {}
+
+function getKeyNotes() {
+	const keyNotes = [];
+	const schmuckler = new Schmuckler(this.melody);
+	const tonality = schmuckler.krumhanslSchmuckler();
+	const tonicIndex = NOTE_NAMES.findIndex((elem) => elem === tonality.tonic);
+
+	if (tonality.isMajor) {
+		for (let i = 0; i < 7; ++i) {
+			keyNotes.push(
+				NOTE_NAMES[(tonicIndex + this.majorKey[i]) % NOTE_NAMES.length]
+			);
+		}
+	} else {
+		for (let i = 0; i < 7; ++i) {
+			keyNotes.push(
+				NOTE_NAMES[(tonicIndex + this.minorKey[i]) % NOTE_NAMES.length]
+			);
+		}
+	}
+
+	return keyNotes;
 }
 
 function sharpToAbc(noteName) {
--- api/logic/melodies/melody.js
+++ api/logic/melodies/melody.js
@@ -36,7 +36,7 @@ function harmonize(options) {
 	console.log(harmonizer.getChordsAbc());
 	// The magic of harmonization later
 
-	return harmonizer.getArpeggioAbc();
+	return harmonizer.getChordsAbc();
 }
 
 function getAbc() {
