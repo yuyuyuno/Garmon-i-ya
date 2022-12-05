94130055b74f0218af3f6bb8d0af14d3df13e0d0
yuyuyuno <fuwafuwayuya@gmail.com>
2022-12-05 03:49:11 +0300

    user manual added

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,34 @@ import React, { useState } from 'react';
 
 import { MelodyInput } from './pages/MelodyInput';
 import { MelodyOutput } from './pages/MelodyOutput';
+import { UserManual } from './pages/UserManual';
 
 const App = () => {
 	const [appState, setAppState] = useState('input');
 	const [outputContent, setOutputContent] = useState(null);
+	const goBack = () => {
+		setAppState('input');
+	};
 
 	return (
 		<div>
+			{appState === 'manual' && <UserManual goBack={goBack} />}
 			{appState === 'input' && (
 				<MelodyInput
 					handleGetHarmonized={(harmonizationResult) => {
 						setOutputContent(harmonizationResult);
 						setAppState('output');
 					}}
+					goToTheManual={() => {
+						setAppState('manual');
+					}}
 				/>
 			)}
 			{appState === 'output' && outputContent.status === 'ok' && (
 				<MelodyOutput
 					results={outputContent.sheets}
-					goBack={() => {
-						setAppState('input');
-					}}
-					retry={() => {}}
+					goBack={goBack}
+					//	retry={() => {}}
 				/>
 			)}
 		</div>
